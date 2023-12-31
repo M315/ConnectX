2810276d8591319f994b6d4df2cdfd49c19f059f
M315 <marc.jovanibertran@gmail.com>
2023-12-31 19:47:10 +0100

    MCTS

--- src/connectx.py
+++ src/connectx.py
@@ -13,121 +13,21 @@
 # limitations under the License.
 
 import json
+import tqdm
 from os import path
 from random import choice
+from kaggle_environments import make
+
+from utils import play, is_win
+from minimax import minimax_agent
+from mcts import mcts_agent
 
 EMPTY = 0
-
-
-def play(board, column, mark, config):
-    columns = config.columns
-    rows = config.rows
-    row = max([r for r in range(rows) if board[column + (r * columns)] == EMPTY])
-    board[column + (row * columns)] = mark
-
-
-def is_win(board, column, mark, config, has_played=True):
-    columns = config.columns
-    rows = config.rows
-    inarow = config.inarow - 1
-    row = (
-        min([r for r in range(rows) if board[column + (r * columns)] == mark])
-        if has_played
-        else max([r for r in range(rows) if board[column + (r * columns)] == EMPTY])
-    )
-
-    def count(offset_row, offset_column):
-        for i in range(1, inarow + 1):
-            r = row + offset_row * i
-            c = column + offset_column * i
-            if (
-                r < 0
-                or r >= rows
-                or c < 0
-                or c >= columns
-                or board[c + (r * columns)] != mark
-            ):
-                return i - 1
-        return inarow
-
-    return (
-        count(1, 0) >= inarow  # vertical.
-        or (count(0, 1) + count(0, -1)) >= inarow  # horizontal.
-        or (count(-1, -1) + count(1, 1)) >= inarow  # top left diagonal.
-        or (count(-1, 1) + count(1, -1)) >= inarow  # top right diagonal.
-    )
-
 
 def random_agent(obs, config):
     return choice([c for c in range(config.columns) if obs.board[c] == EMPTY])
 
-
-def negamax_agent(obs, config):
-    columns = config.columns
-    rows = config.rows
-    size = rows * columns
-
-    # Due to compute/time constraints the tree depth must be limited.
-    max_depth = 4
-
-    def negamax(board, mark, depth):
-        moves = sum(1 if cell != EMPTY else 0 for cell in board)
-
-        # Tie Game
-        if moves == size:
-            return (0, None)
-
-        # Can win next.
-        for column in range(columns):
-            if board[column] == EMPTY and is_win(board, column, mark, config, False):
-                return ((size + 1 - moves) / 2, column)
-
-        # Recursively check all columns.
-        best_score = -size
-        best_column = None
-        for column in range(columns):
-            if board[column] == EMPTY:
-                # Max depth reached. Score based on cell proximity for a clustering effect.
-                if depth <= 0:
-                    row = max(
-                        [
-                            r
-                            for r in range(rows)
-                            if board[column + (r * columns)] == EMPTY
-                        ]
-                    )
-                    score = (size + 1 - moves) / 2
-                    if column > 0 and board[row * columns + column - 1] == mark:
-                        score += 1
-                    if (
-                        column < columns - 1
-                        and board[row * columns + column + 1] == mark
-                    ):
-                        score += 1
-                    if row > 0 and board[(row - 1) * columns + column] == mark:
-                        score += 1
-                    if row < rows - 2 and board[(row + 1) * columns + column] == mark:
-                        score += 1
-                else:
-                    next_board = board[:]
-                    play(next_board, column, mark, config)
-                    (score, _) = negamax(next_board,
-                                         1 if mark == 2 else 2, depth - 1)
-                    score = score * -1
-                if score > best_score or (score == best_score and choice([True, False])):
-                    best_score = score
-                    best_column = column
-
-        return (best_score, best_column)
-
-    _, column = negamax(obs.board[:], obs.mark, max_depth)
-    if column == None:
-        column = choice([c for c in range(columns) if obs.board[c] == EMPTY])
-    return column
-
-
-agents = {"random": random_agent, "negamax": negamax_agent}
-
+agents = {"random": random_agent, "minimax": minimax_agent}
 
 def interpreter(state, env):
     columns = env.configuration.columns
@@ -210,3 +110,26 @@ def html_renderer():
     jspath = path.abspath(path.join(dirpath, "connectx.js"))
     with open(jspath) as f:
         return f.read()
+
+def main():
+    env = make("connectx", debug=True)
+
+    # Training agent in first position (player 1) against the default random agent.
+    trainer = env.train([None, "negamax"])
+    config = env.configuration
+
+    res = [0, 0, 0]
+    for _ in tqdm.tqdm(range(100)):
+        obs = trainer.reset()
+        while True:
+            env.render()
+            action = mcts_agent(obs, config)
+            obs, reward, done, info = trainer.step(action)
+            if done:
+                res[reward + 1] += 1
+                break
+
+    print(res)
+
+if __name__ == "__main__":
+    main()
--- src/mcts.py
+++ src/mcts.py
@@ -1,69 +1,12 @@
-from typing import Optional
 
 import random
 import math
 import time
 
-EMPTY = 0
-
-def legal_moves(board, config):
-    return [col for (col, cell) in enumerate(board[:config.columns]) if cell == EMPTY]
-
-def play(board, column, mark, config):
-    columns = config.columns
-    rows = config.rows
-    row = max([r for r in range(rows) if board[column + (r * columns)] == EMPTY])
-    board[column + (row * columns)] = mark
+from utils import legal_moves, play, game_result
 
 
-def is_win(board, column, mark, config, has_played=True):
-    columns = config.columns
-    rows = config.rows
-    inarow = config.inarow - 1
-    row = (
-        min([r for r in range(rows) if board[column + (r * columns)] == mark], default=None)
-        if has_played
-        else max([r for r in range(rows) if board[column + (r * columns)] == EMPTY])
-    )
-    if row is None:
-        return False
-
-    def count(offset_row, offset_column):
-        for i in range(1, inarow + 1):
-            r = row + offset_row * i
-            c = column + offset_column * i
-            if (
-                r < 0
-                or r >= rows
-                or c < 0
-                or c >= columns
-                or board[c + (r * columns)] != mark
-            ):
-                return i - 1
-        return inarow
-
-    return (
-        count(1, 0) >= inarow  # vertical.
-        or (count(0, 1) + count(0, -1)) >= inarow  # horizontal.
-        or (count(-1, -1) + count(1, 1)) >= inarow  # top left diagonal.
-        or (count(-1, 1) + count(1, -1)) >= inarow  # top right diagonal.
-    )
-
-def has_won(board, mark, config):
-    won = False
-    for col in range(config.columns):
-        won |= is_win(board, col, mark, config, has_played=True)
-    return won
-
-def game_result(board, config) -> Optional[dict]:
-    if all([cell != EMPTY for cell in board]):
-        return {1: 0.5, 2: 0.5}
-    if has_won(board, 1, config):
-        return {1: 1., 2: 0.}
-    if has_won(board, 2, config):
-        return {1: 0., 2: 1.}
-    return None
-
+ITER_MAX = 10
 
 class MCTSNode:
     def __init__(self, board, config, turn, move=None, parent=None):
@@ -132,4 +75,3 @@ def mcts_agent(obs, config):
     root = MCTSNode(board=obs["board"], config=config, turn=obs["mark"])
     best_move = MCTS(root)
     return best_move
-
